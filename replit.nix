4c645361e560d59c74d89f7dad946e4afe206b44
Mediascape TV <36661808-mediascapetv@users.noreply.replit.com>
2025-07-07 17:27:13 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python311
-    pkgs.python311Packages.fastapi
-    pkgs.python311Packages.pandas
-    pkgs.python311Packages.jinja2
-    pkgs.python311Packages.uvicorn
-  ];
-}
